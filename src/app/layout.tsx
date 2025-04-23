0956b109267226bcca3650e6c87a0758fc739d7d
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-04-23 21:19:30 +0530

    Feat: Code cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import React from 'react'
 
 const poppins = Poppins({
   subsets: ['latin'],
