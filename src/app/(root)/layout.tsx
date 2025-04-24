24d0e4c9cdd8c6de3c1447ca3172fc021d3a79a8
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-04-24 17:22:45 +0530

    Sidebar Init

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
+import { ModeToggle } from '@/components/ui/mode-toggle'
+import { Separator } from '@/components/ui/separator'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">
+        <header className="flex items-center justify-between p-1">
+          <SidebarTrigger className="size-9 cursor-pointer" size="lg" />
+          <ModeToggle />
+        </header>
+        <Separator />
+        <section className="p-2" aria-label="main">
+          {children}
+        </section>
+      </main>
+    </SidebarProvider>
+  )
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,102 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem
+} from '@/components/ui/sidebar'
+import {
+  ChevronRight,
+  GithubIcon,
+  Home,
+  InstagramIcon,
+  LinkedinIcon,
+  MessageSquareIcon,
+  TwitterIcon
+} from 'lucide-react'
+import Link from 'next/link'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
+
+export function AppSidebar() {
+  const contactItems = [
+    {
+      title: 'GitHub',
+      url: 'https://github.com/duggavamshidhar/online-text-tools',
+      icon: GithubIcon,
+      targetBlank: true
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://linkedin.com/in/duggavamshidhar/',
+      icon: LinkedinIcon,
+      targetBlank: true
+    },
+    {
+      title: 'X (Twitter)',
+      url: 'https://x.com/duggavamshidhar/',
+      icon: TwitterIcon,
+      targetBlank: true
+    },
+    {
+      title: 'Instagram',
+      url: 'https://instagram.com/duggavamshidhar/',
+      icon: InstagramIcon,
+      targetBlank: true
+    },
+    {
+      title: 'Bluesky',
+      url: 'https://bsky.app/profile/duggavamshidhar.bsky.social',
+      icon: MessageSquareIcon,
+      targetBlank: true
+    }
+  ]
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/">
+                    <Home size="28" />
+                    <span>Home</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton className="flex w-full cursor-pointer items-center justify-between">
+                      Contact
+                      <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {contactItems.map((item) => (
+                        <SidebarMenuSubItem key={item.title}>
+                          <SidebarMenuButton asChild>
+                            <Link href={item.url} target={item.targetBlank ? '_blank' : '_self'}>
+                              <item.icon />
+                              <span>{item.title}</span>
+                            </Link>
+                          </SidebarMenuButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            </SidebarMenu>
+          </SidebarContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
