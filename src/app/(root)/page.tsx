75b284a5e2a1f7b88399f93d09107b90e96248c9
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-05-03 12:56:30 +0530

    Word and character counted implementation

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,19 @@
 'use client'
 
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
-import { ClipboardCopy, ClipboardPaste, Download, ListRestart, Scissors } from 'lucide-react'
-import { useState } from 'react'
+import {
+  CaseLower,
+  ClipboardCopy,
+  ClipboardPaste,
+  Download,
+  ListRestart,
+  Scissors,
+  WholeWord
+} from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 
 export default function Page() {
@@ -13,6 +22,23 @@ export default function Page() {
     start: 0,
     end: 0
   })
+  const [wordCount, setWordCount] = useState<number>(0)
+  const [characterCount, setCharacterCount] = useState<number>(0)
+
+  useEffect(() => {
+    function handleCharacterCount() {
+      setCharacterCount(text.length)
+    }
+    function handleWordCount() {
+      const words = text
+        .trim()
+        .split(/\s+/)
+        .filter((word) => word.length > 0)
+      setWordCount(words.length)
+    }
+    handleCharacterCount()
+    handleWordCount()
+  }, [text])
 
   function handleCursorPosition(e: React.ChangeEvent<HTMLTextAreaElement>) {
     const { selectionStart, selectionEnd } = e.target
@@ -47,6 +73,7 @@ export default function Page() {
       .then(() => toast.success('Copied to clipboard successfully.'))
       .finally(() => setText(''))
   }
+
   function resetTextArea() {
     setText('')
   }
@@ -67,92 +94,134 @@ export default function Page() {
       <div className="mx-auto flex max-w-3xl flex-col gap-y-2">
         <div className="px-0.5 text-2xl font-semibold">Notepad</div>
         <div className="flex flex-col gap-y-2">
-          <div className="flex w-full justify-end gap-x-1 rounded border p-1">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    onClick={downloadAsFile}
-                    variant="outline"
-                    className="cursor-pointer"
-                    size="icon"
-                  >
-                    <Download size="28" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Download as text file</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    onClick={cutToClipboard}
-                    variant="outline"
-                    className="cursor-pointer"
-                    size="icon"
-                  >
-                    <Scissors size="28" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Cut</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    onClick={copyToClipboard}
-                    variant="outline"
-                    className="cursor-pointer"
-                    size="icon"
-                  >
-                    <ClipboardCopy size="28" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Copy to clipboard</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    variant="outline"
-                    className="cursor-pointer"
-                    size="icon"
-                    onClick={pasteToClipboard}
-                  >
-                    <ClipboardPaste size="28" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Paste</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    onClick={resetTextArea}
-                    variant="outline"
-                    className="cursor-pointer"
-                    size="icon"
-                  >
-                    <ListRestart size="28" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Reset</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+          <div className="flex w-full items-center justify-between rounded border p-1">
+            <div className="flex gap-x-1">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      className="flex w-auto justify-between gap-1 p-2"
+                      variant="outline"
+                      size="icon"
+                    >
+                      <CaseLower size="28" />
+                      <span>
+                        <Badge variant="secondary">{characterCount}</Badge>
+                      </span>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Character count</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      className="flex w-auto justify-between gap-1 p-2"
+                      variant="outline"
+                      size="icon"
+                    >
+                      <WholeWord size="28" />
+                      <span>
+                        <Badge variant="secondary">{wordCount}</Badge>
+                      </span>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Word count</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+            <div className="flex gap-x-1">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={downloadAsFile}
+                      variant="outline"
+                      className="cursor-pointer"
+                      size="icon"
+                    >
+                      <Download size="28" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Download as text file</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={cutToClipboard}
+                      variant="outline"
+                      className="cursor-pointer"
+                      size="icon"
+                    >
+                      <Scissors size="28" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Cut</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={copyToClipboard}
+                      variant="outline"
+                      className="cursor-pointer"
+                      size="icon"
+                    >
+                      <ClipboardCopy size="28" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Copy to clipboard</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="outline"
+                      className="cursor-pointer"
+                      size="icon"
+                      onClick={pasteToClipboard}
+                    >
+                      <ClipboardPaste size="28" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Paste</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={resetTextArea}
+                      variant="outline"
+                      className="cursor-pointer"
+                      size="icon"
+                    >
+                      <ListRestart size="28" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Reset</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
           </div>
           <div>
             <Textarea
