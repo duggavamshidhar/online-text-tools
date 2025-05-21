c0ba584f76b71afdf99a92cfbf6b1d63d57f0772
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-05-21 18:42:30 +0530

    Fix typo in page title for Base64 encode/decode tool
    Updated the title metadata to correctly read "Base64 Encode and Decode" instead of "Base64 Encode and Decoder." This ensures consistency and improves clarity for users.

--- src/app/(root)/text-tools/base64-encode-and-decode/page.tsx
+++ src/app/(root)/text-tools/base64-encode-and-decode/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import Base64EncodeAndDecodeComponent from '@/components/base64-encode-and-decode-component'
 
 export const metadata: Metadata = {
-  title: 'Base64 Encode and Decoder'
+  title: 'Base64 Encode and Decode'
 }
 
 export default function Base64EncodeAndDecode() {
